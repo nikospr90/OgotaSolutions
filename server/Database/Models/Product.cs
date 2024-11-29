8657eed40c32b791491dcb8e09b4fbb47c9d86a8
Johodah <johnny.dahle@hotmail.com>
2024-11-29 10:55:13 +0100

    Created database, remoduled map structure.

---
+++ server/Database/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace server.Database.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Location { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public DateOnly UpdatedAt { get; set; }
+    }
+}
---
+++ server/Database/OgotaContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Database.Models;
+
+namespace server.Database
+{
+    public class OgotaContext : DbContext
+    {
+        public OgotaContext(DbContextOptions<OgotaContext> option) : base (option)
+        {
+        }
+
+        public DbSet <Product> Products { get; set; }
+        public DbSet <Customer> Customers { get; set; }
+        public DbSet <Admin> Admins { get; set; }
+        public DbSet <OrderHistory> OrderHistories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ogota.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        ProductName = "Programmering Grund 1",
+                        ProductDescription = "Lär dig grunderna i programmering",
+                        Price = 500,
+                        Stock = 30,
+                        Location = "Distans",
+                        CreatedAt = new DateOnly(2021, 10, 10),
+                        UpdatedAt = new DateOnly(2021, 10, 10)
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        ProductName = "HTML",
+                        ProductDescription = "Lär dig mer om HTML",
+                        Price = 1000,
+                        Stock = 30,
+                        Location = "Distans",
+                        CreatedAt = new DateOnly(2022, 10, 10),
+                        UpdatedAt = new DateOnly(2022, 10, 10)
+                    });
+        }
+    }
+}
